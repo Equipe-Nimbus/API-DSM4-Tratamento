3447c6062315b91dd1ba82b930426282a10269b4
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-22 14:00:10 -0300

    Feat: Rota que recebe o aviso do servico de recepcao (#2)

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { config } from 'dotenv';
+import tratamentoRoutes from "./routes/TratamentoRoutes";
+import cors from "cors";
+
+config();
+const app = express();
+app.use(express.json());
+const corsOptions = {
+    origin: ['http://localhost:8001'],
+    optionsSuccessStatus: 200 
+  };
+  
+app.use(cors(corsOptions));
+
+app.use("/tratamento", tratamentoRoutes)
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
