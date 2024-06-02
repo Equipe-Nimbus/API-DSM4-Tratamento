f6d31bc8c4a5ee4a85404c3a1155ca7a2f7cbab0
Bruno Denardo <brunoden.bd@gmail.com>
2024-06-02 17:41:51 -0300

    Fix(tratamento): Adicionando tratamento de erros EN-217

--- src/Controllers/TratamentoController.ts
+++ src/Controllers/TratamentoController.ts
@@ -3,15 +3,27 @@ import PegaDadosMedicoes from "../services/PegaDadosMedicoes";
 import TrataMedicao from "../services/TrataMedicao";
 import MedicaoMongo from "../interfaces/MedicaoMongo";
 import PgDataSource from "../ConfigPostgres";
+import DeletaDadosMedicoes from "../services/DeletaDadosMedicoes";
 
 
 class TratamentoController{
 
     async tratar(req:Request, res:Response){
         let medicoes = await PegaDadosMedicoes.pegar() as MedicaoMongo[]
-        console.log("OQ FOI PEGO: ", medicoes)
-        await TrataMedicao.tratar(medicoes)
-        res.send("Aviso recebido")
+        try {
+            console.log("OQ FOI PEGO: ", medicoes)
+            await TrataMedicao.tratar(medicoes)
+            await DeletaDadosMedicoes.deletar(medicoes);
+            res.send("Aviso recebido")
+        } catch (error) {
+            if(error.tipo === "Deleção"){
+                res.status(500).send("Erro na Deleção")
+                await DeletaDadosMedicoes.deletar(medicoes)
+            }
+            else{
+                res.status(500).send("Erro no tratamento")
+            }
+        }
     }
 
 }
